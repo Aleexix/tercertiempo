450721abe7c4ef6302e0edb7ec5c8e83f3a1b41a
Alexxix <aorostegui2@gmail.com>
2026-01-11 01:33:38 -0500

    home-page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['var(--font-bebas)'],
+        sans: ['var(--font-inter)'],
+      },
+    },
+  },
+  plugins: [],
 };
 
 export default config;
